b3b8354a610b63c8570987804006b3abc84d2795
tejassoni5090 <tejassoni5090@gmail.com>
2026-07-08 18:56:03 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Tejas community",
         // Activity type number (0 = Playing).
         type: 0,
       },
@@ -91,8 +91,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#18C6EB",
+      secondary: "#9E84AC",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
